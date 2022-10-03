66579116cba97b82a3df83e0ab7571bb570475f4
Nelician <wooki9709@gmail.com>
2022-10-03 20:57:54 +0900

    fs 모듈을 이용한 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,12 +1,21 @@
+const fs = require("fs").promises;
+
 class UserStorage{
-    static #users = {
-        id : ["woorimIT", "나개발", "김팀장"],
-        psword: ["1234", "1234", "123456"],
-        name : ["우리밋", "나개발", "김팀장"]
+    
+    static #getUserInfo(data ,id){
+        const users = (JSON.parse(data));
+            const idx = users.id.indexOf(id);
+            const usersKeys = Object.keys(users);
+            const userInfo = usersKeys.reduce((newUser, info)=>{
+            newUser[info] = users[info][idx];
+            return newUser;
+            },{});
+
+            return userInfo;
     };
 
     static getUsers(...fields){
-        const users = this.#users;
+        //const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -16,17 +25,17 @@ class UserStorage{
         return newUsers;
     }
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info)=>{
-            newUser[info] = users[info][idx];
-            return newUser;
-        },{});
-        return userInfo;
+       // const users = this.#users;
+       return fs.readFile("./src/database/users.json")
+       .then((data) => {
+            return this.#getUserInfo(data, id);
+       })
+        .catch(console.error); 
     }
+    
+
     static save(userInfo){
-        const users = this.#users;
+       // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
